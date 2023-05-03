64216a8394942f981fb2c79f646b0213daae9a17
Octagon <ugorji757@gmail.com>
2023-05-03 03:57:02 +0100

    progress with landing page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,13 @@
 // import React from "react";
 // import { Helmet } from "react-helmet";
 
-export default function Home(){
-    
-    return(
+export default function Home() {
+
+    return (
         <main>
             <section className="hero">
                 <div className="hero-body p-0">
-                    <div className="container is-fluid columns  is-flex-direction-row-reverse" style={{ top: '2%' }}>
+                    <div className="container is-fluid columns is-flex-direction-row-reverse" style={{ top: '2%' }}>
                         <div className="column text-center">
                             <img src="/hero-man.png" />
                         </div>
@@ -28,13 +28,13 @@ export default function Home(){
             <section className="section is-medium">
                 <div className="columns">
                     <div className="column is-4">
-                    <section className="section-content">
-                        <p className="text-tertiary mb-3">SERVICES</p>
-                        <h3 className="title is-3">Our Capabilities</h3>
-                    </section>
-                    <div className="section-content">
-                        <p className="content text-gray">We will bring the breathe of our experience and industry knowledge to help you succeed</p>
-                    </div>
+                        <section className="section-content">
+                            <p className="text-tertiary mb-3">SERVICES</p>
+                            <h3 className="title is-3">Our Capabilities</h3>
+                        </section>
+                        <div className="section-content">
+                            <p className="content text-gray">We will bring the breathe of our experience and industry knowledge to help you succeed</p>
+                        </div>
                     </div>
                     <div className="column is-8">
                         <div className="columns">
@@ -47,19 +47,55 @@ export default function Home(){
                             </div>
                             <div className="column is-4">
                                 <p className="mb-3">
-                                <i className="fa-duotone fa-2x fa-lightbulb-on ico-green"></i>
+                                    <i className="fa-duotone fa-2x fa-lightbulb-on ico-green"></i>
                                 </p>
                                 <h5 className="title is-5">Promoting Innovation</h5>
                                 <p className="text-gray">Companies constantly seeking methods to optimize and simplify processes also contribute to various innovations.</p>
                             </div>
                             <div className="column is-4">
-                            <p className="mb-3">
-                            <i className="fa-duotone fa-2x fa-chart-mixed ico-red"></i>
-                            </p>
+                                <p className="mb-3">
+                                    <i className="fa-duotone fa-2x fa-chart-mixed ico-red"></i>
+                                </p>
                                 <h5 className="title is-5">Increase In Productivity</h5>
                                 <p className="text-gray">Increase the productivity of your brand by up to 120%.</p>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section bg-pri-dark">
+                <div className="columns is-flex-direction-arow-reverse">
+                    <div className="column text-center">
+                        <img className="why-woman" src="/why-woman.png" />
+                    </div>
+                    <div className="column is-align-self-center">
+                        <section className="section-content">
+                            <p className="text-tertiary mb-3">WHY CHOOSE US</p>
+                            <h3 className="title is-3 has-text-white space-2x">Get your business ready For the complete Tech Takeover</h3>
+                        </section>
+                        <section className="section-content">
+                            <div className="mb-3">
+                            <p className="list-flex">
+                                <span className="icon">
+                                    <i className="fs-one-half fa-duotone fa-circle-check"></i>
+                                </span><span className="text-ash">120% Increase in Productivity</span></p>
+                            </div>
+                            <div className="mb-3">
+                            <p className="list-flex">
+                                <span className="icon">
+                                    <i className="fs-one-half fa-duotone fa-circle-check"></i>
+                                </span><span className="text-ash">Development of Technical Models</span></p>
+                            </div>
+                            <div className="mb-3">
+                            <p className="list-flex">
+                                <span className="icon">
+                                    <i className="fs-one-half fa-duotone fa-circle-check"></i>
+                                </span><span className="text-ash">Better Customer Experience</span></p>
+                            </div>
+                            <section className="section-content">
+                                <a href="/" className="button btn-cta is-light fw-bold is-rounded">Get Started</a>
+                            </section>
+                        </section>
                     </div>
                 </div>
             </section>
