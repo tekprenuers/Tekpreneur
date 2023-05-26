381a408dcf9092baf9c265706cf1c5413e878db5
Octagon <ugorji757@gmail.com>
2023-05-26 06:39:30 +0100

    updated quote section

--- src/Pages/Quote.jsx
+++ src/Pages/Quote.jsx
@@ -6,25 +6,37 @@ export default function Quote(){
         <>
             <section className="section">
                 <form>
+                    <section className="section-content">
+                        <h3 className="title is-3 mb-1">Contact Information</h3>
+                        <p>Tell us more about you</p>                      
+                    </section>
+<div className="field">
+                            <label>Full Name</label>
+                            <input className="input" octavalidate="R,NAME" id="inp_name" />
+                        </div>
                     <div className="field">
-                        <label>Full Name</label>
+                        <label>Email</label>
                         <input className="input" octavalidate="R,NAME" id="inp_name" />
                     </div>
                     <div className="field">
-                        <label>Full Name</label>
+                        <label>Phone Number</label>
                         <input className="input" octavalidate="R,NAME" id="inp_name" />
                     </div>
                     <div className="field">
-                        <label>Full Name</label>
+                        <label>Brand/Company Name</label>
+                        <input className="input" octavalidate="R,NAME" id="inp_name" />
+                    </div>
+                    <hr />
+                    <section className="section-content">
+                        <h3 className="title is-3 mb-1">Project Information</h3>
+                        <p>Tell us more about this project</p>                      
+                    </section>
+                    <div className="field">
+                        <label>Preferred Website Name</label>
                         <input className="input" octavalidate="R,NAME" id="inp_name" />
                     </div>
                     <div className="field">
-                        <label>Full Name</label>
-                        <input className="input" octavalidate="R,NAME" id="inp_name" />
-                    </div>
-                    <div className="field">
-                        <label>Full Name</label>
-                        <input className="input" octavalidate="R,NAME" id="inp_name" />
+                        <label><input type="checkbox" />Web Design & Development</label>
                     </div>
                 </form>
             </section>
